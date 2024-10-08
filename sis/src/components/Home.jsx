8969aa87d0ab209082fd0f881aa77774be539aae
Faizyzaidi <faizyzaidi80@gmail.com>
2024-10-08 05:25:54 +0000

    desinging start

--- sis/src/components/Home.jsx
+++ sis/src/components/Home.jsx
@@ -1,10 +1,13 @@
 // src/components/Home.js
 import React from 'react';
+import background from "../assets/background.png";
 
 function Home() {
   return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <h1 className="text-5xl font-bold">Welcome to Student Information System</h1>
+    <div
+      className="flex justify-center items-center h-screen bg-white-100">
+        <img className='h-79 ' src={background} alt=""  />
+      <h1 className="text-5xl font-bold text-blue-800">Welcome to Student Information System</h1>
     </div>
   );
 }
